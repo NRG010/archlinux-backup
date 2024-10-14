96b35081b0d186c00b04006339c4870f9352d9a2
Baldev Singh <personalemailofbaldev@gmail.com>
2024-10-14 19:02:58 +0530

    some fixes

---
+++ nvim/lua/chadrc.lua
@@ -0,0 +1,12 @@
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+	theme = "catppuccin",
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+	},
+}
+
+return M
